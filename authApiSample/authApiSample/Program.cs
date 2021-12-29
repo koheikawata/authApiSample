9023bdfc529e001198d612295fd0fc36fe9db72a
Kohei Kawata <kokawata@microsoft.com>
2021-12-29 11:43:33 +0900

    Fix AzureAd section

--- authApiSample/authApiSample/Program.cs
+++ authApiSample/authApiSample/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAD"));
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
